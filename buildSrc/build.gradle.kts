ef60ca1f3a3611cf2c585dada0504991fa0863ad
Rajat Sahu <67316346+Coolboyrajat@users.noreply.github.com>
2025-03-22 13:38:20 +0530

    initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins{
+    `kotlin-dsl`
+}
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+dependencies{
+    implementation(libs.pluginKotlin)
+    implementation(libs.pluginComposeCompiler)
+    implementation(libs.pluginKsp)
+    implementation(libs.pluginSerialization)
+    implementation(libs.pluginComposeMultiplatform)
+    implementation(libs.pluginChangeLog)
+    implementation(libs.pluginBuildConfig)
+    implementation(libs.pluginAboutLibraries)
+    implementation(libs.semver)
+    implementation("ir.amirab.util:platform:1")
+    implementation("ir.amirab.plugin:git-version-plugin:1")
+    implementation("ir.amirab.plugin:installer-plugin:1")
+}
